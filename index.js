785b6185e8a568a8f62c90524bd7cb156d9e6071
kate-miah <ekaterin89@gmail.com>
2025-09-02 21:48:16 -0500

    apply, call, bind

--- index.js
+++ index.js
@@ -33,3 +33,56 @@ const arrow = () => {
 }
 
 arrow();
+
+const newspaper = {
+    title: 'Our super-newspaper',
+    articles: [{
+        author: 'John Dow',
+        date: 19-10-2023,
+        text: 'lorem ipsum'
+    }, {
+        author: 'Jane Dow',
+        date: 19-10-2023,
+        text: 'dolor sit'
+    }, {
+        author: 'Rick Dow',
+        date: 19-10-2023,
+        text: 'amet amen'
+    }, {
+        author: 'ole Dow',
+        date: 19-10-2023,
+        text: 'lorem ipsum'
+    }, {
+        author: 'Nick Dow',
+        date: 19-10-2023,
+        text: 'ipsum loren'
+    }], 
+    showArticles: function() {
+        this.articles.forEach((currentArticle) => {
+            console.log(`${this.title} --- ${currentArticle.author} - ${currentArticle.text}`);
+        })
+    }
+}
+
+
+// Передача контекста
+
+const auto = {
+    model: 'BMW',
+    speed: 150
+}
+function sayVxh(a, b) {
+    console.log(`${this.model} делает вжжж со скоростью ${this.speed}`);
+    console.log('А тем временем a+b = ', a+b)
+}
+sayVxh.apply(auto, [2, 3]); // вызивает функцию с оьъектом auto  в качестве this
+sayVxh.call(auto, 4, 5);
+
+/* 
+apply(this, [argument]) - метод функции, который вызывает функцию, передает ее в качестве this первым аргументом и второй аргумент - масив - предается параметрами функции
+
+call(this, arg1, arg2, arg3, ...) - метод функции, который вызывает функцию, передает ее в качестве this первым аргументом, все остальные аргуенты после первого (поштучно) передает параметрами функии
+
+*/
+
+const wrappedFunction = sayVxh.bind(auto) // оборачивает вызов йункции в контексте, возвращает ссылкку (посилання) на готовую к работе функцию, которой this = auto 
